1339ab2685035629586229da1389ca9aa52e7095
shiraji <isogai.shiraji@gmail.com>
2017-07-21 15:31:16 +0900

    initial commit

---
+++ app/src/main/java/com/github/shiraji/myapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.github.shiraji.myapplication
+
+import android.Manifest
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.OnShowRationale
+import permissions.dispatcher.PermissionRequest
+import permissions.dispatcher.RuntimePermissions
+
+@RuntimePermissions(kotlin = true)
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    fun foo() {
+
+    }
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    fun foo2(request: PermissionRequest) {
+
+    }
+}
